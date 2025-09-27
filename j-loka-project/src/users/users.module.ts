efb356a309e651e4a0cbf98ef144478251e3d53b
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-09-27 17:52:28 +0300

    Define users controller

--- j-loka-project/src/users/users.module.ts
+++ j-loka-project/src/users/users.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
 
-@Module({})
+@Module({
+  // eslint-disable-next-line prettier/prettier
+  controllers: [UsersController]
+})
 export class UsersModule {}
