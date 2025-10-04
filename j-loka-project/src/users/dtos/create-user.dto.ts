365f715879d8fa6361921453a11fd4751d776f4a
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-04 15:08:12 +0300

    Build user create dto with class-validator

---
+++ j-loka-project/src/users/dtos/create-user.dto.ts
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsEmail, IsNotEmpty, IsOptional, IsString, Length, max, min } from 'class-validator'
+
+export class CreateUserDto {
+	@IsString()
+	@IsNotEmpty()
+	@Length(3, 255)
+  firstName: string;
+
+	@IsString()
+	@IsOptional()
+	@Length(3, 255)
+  lastName?: string;
+  
+	@IsEmail()
+	@IsNotEmpty()
+	email: string;
+  
+	@IsString()
+	@IsNotEmpty()
+	@Length(5, 150)
+	password: string;  
+}
