477aa67e6da78f751aa76d2f1fb2cb31c3bbcdf0
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-18 12:45:46 +0300

    Adding users service

--- j-loka-project/src/users/users.module.ts
+++ j-loka-project/src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 @Module({
   // eslint-disable-next-line prettier/prettier
-  controllers: [UsersController]
+  controllers: [UsersController],
+  providers: [UsersService]
 })
 export class UsersModule {}
