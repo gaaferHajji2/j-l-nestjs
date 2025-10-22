1e8763500c93e43110159204714c564b8920c7db
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-22 04:55:53 +0300

    Build posts methods for get only

--- j-loka-project/src/posts/posts.controller.ts
+++ j-loka-project/src/posts/posts.controller.ts
@@ -1,8 +1,18 @@
 /* eslint-disable prettier/prettier */
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Param } from '@nestjs/common'
 import { PostsService } from './posts.service'
 
 @Controller('posts')
 export class PostsController {
     constructor(private readonly postsService: PostsService) {}
+
+    @Get('/:userId')
+    public getAllPostsForUser(@Param('userId') userId: string) {
+        return this.postsService.getAllPostsForUser(userId);
+    }
+
+    @Get('/')
+    public getAllPosts() {
+        return this.postsService.getAllPosts();
+    }
 }
--- j-loka-project/src/posts/posts.service.ts
+++ j-loka-project/src/posts/posts.service.ts
@@ -4,7 +4,37 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class PostsService {
 
-    public getAllPosts() {}
+    public getAllPostsForUser(userId: string) {
+        return [{
+            'name': 'Post-01',
+            userId,
+            'date': '2000-01-01'
+        }, {
+            'name': 'Post-02',
+            userId,
+            'date': '2000-01-01'
+        }, {
+            'name': 'Post-03',
+            userId,
+            'date': '2000-01-01'
+        }]
+    }
+
+    public getAllPosts() {
+        return [{
+            'name': 'Post-01',
+            userId: 123,
+            'date': '2000-01-01'
+        }, {
+            'name': 'Post-02',
+            userId: 456,
+            'date': '2000-01-01'
+        }, {
+            'name': 'Post-03',
+            userId: 789,
+            'date': '2000-01-01'
+        }]
+    }
 
     public getPostById() {}
 
