7ad41394678d861459337e5e3b0ef84ee91d6ef7
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-23 04:35:24 +0300

    Build basic config for swagger

--- j-loka-project/src/main.ts
+++ j-loka-project/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /**
+   * For Validation And Requests With transform
+   */
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +16,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  /**
+   * For Swagger Building
+   */
+  const builder = new DocumentBuilder().setVersion('1.0').build();
+
+  const doc = SwaggerModule.createDocument(app, builder);
+  // the first param is the path to document
+  SwaggerModule.setup('docs', app, doc);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
