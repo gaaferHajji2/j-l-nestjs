d99ffb2d925051294fa406caa7319cd516f2be1b
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-08 03:06:06 +0300

    Adding simple validation to post create

--- j-loka-project/src/posts/dto/create-post.dto.ts
+++ j-loka-project/src/posts/dto/create-post.dto.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import { IsNotEmpty, IsString, Length } from "class-validator";
+import { IsArray, IsDate, IsEnum, IsNotEmpty, IsOptional, IsString, IsUrl, Length } from "class-validator";
 import { PostStatus } from "../enum/post-status.enum";
 import { PostType } from "../enum/post-type.enum";
 
@@ -10,13 +10,45 @@ export class CreatePostDto {
     @Length(4, 255)
     @IsNotEmpty()
     title: string;
+
+    @IsEnum(PostType)
+    @IsNotEmpty()
     postType: PostType;
+
+    @IsString()
+    @Length(4, 255)
+    @IsNotEmpty()
     slug: string;
+
+    @IsEnum(PostStatus)
+    @IsNotEmpty()
     status: PostStatus;
+
+    @IsString()
+    @Length(1, 5000)
+    @IsOptional()
     content?: string;
+
+    @IsString()
+    @Length(1, 5000)
+    @IsOptional()
     schema?:string;
+
+    @IsUrl()
+    @IsOptional()
+    @Length(1, 255)
     featuredImageUrl?: string;
+
+    @IsDate()
+    @IsOptional()
     publishedOn?: Date;
+
+    @IsArray()
+    @IsString()
+    @IsOptional()
     tags?: string[];
+
+    @IsArray()
+    @IsOptional()
     metaOptions: object[];
 }
