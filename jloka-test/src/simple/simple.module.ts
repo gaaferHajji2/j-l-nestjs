26f3d21859fbdde5434c0a76ab6d3747cc877a00
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-12-15 12:29:18 +0300

    Finish from resources

--- jloka-test/src/simple/simple.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SimpleService } from './simple.service';
-import { SimpleController } from './simple.controller';
-
-@Module({
-  controllers: [SimpleController],
-  providers: [SimpleService],
-})
-export class SimpleModule {}
