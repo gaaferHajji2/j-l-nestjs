a1f47a8ab0c319d7159826dddb0422e19212895d
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-12-16 15:45:59 +0300

    Update cat controller & use cats service

--- jloka-test/src/cats/cats.controller.ts
+++ jloka-test/src/cats/cats.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+    constructor(private readonly catsService: CatsService) {}
+
+    @Post()
+    async create(@Body() createCatDto: CreateCatDto): Promise<CreateCatDto> {
+        this.catsService.create(createCatDto)
+        return createCatDto
+    }
+
+    @Get()
+    async findAll(): Promise<Cat[]> {
+        return this.findAll();
+    }
+}
