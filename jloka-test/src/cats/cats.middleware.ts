c54b9eb244a85078190dbb721e87f2a70b3f09b5
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-12-18 13:05:42 +0300

    Adding logger middleware

---
+++ jloka-test/src/cats/cats.middleware.ts
@@ -0,0 +1,10 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class CatsMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log(`The request is: ${req.path}, with method: ${req.method}`)
+    next()
+  }
+}
