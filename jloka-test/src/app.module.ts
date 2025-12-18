ee491e37cf773eb205c35c3c8902767963362697
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-12-18 13:12:48 +0300

    apply middleware for get cats only

--- jloka-test/src/app.module.ts
+++ jloka-test/src/app.module.ts
@@ -1,13 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CatsModule } from './cats/cats.module';
+import { CatsMiddleware } from './cats/cats.middleware';
 
 @Module({
   imports: [UsersModule, EventEmitterModule.forRoot(), CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // consumer.apply(CatsMiddleware).forRoutes('cats')
+    consumer.apply(CatsMiddleware).forRoutes({ path: 'cats', method: RequestMethod.GET })
+  }
+}
