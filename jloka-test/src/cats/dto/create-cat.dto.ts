7b101b5ed1b4ad1bb1c7db70d6d27200ec04db57
JafarLoka <gaafer.hajji1995@gmail.com>
2025-12-26 14:06:40 +0300

    Adding validation to create cats dto

--- jloka-test/src/cats/dto/create-cat.dto.ts
+++ jloka-test/src/cats/dto/create-cat.dto.ts
@@ -1,7 +1,9 @@
-
-
+import {IsString, IsInt} from 'class-validator'
 export class CreateCatDto {
+    @IsString()
     name: string;
+    @IsInt()
     age: number;
+    @IsString()
     breed: string;
 }
