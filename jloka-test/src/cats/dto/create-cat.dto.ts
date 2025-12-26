350281e83c58d5840c4f8e77924b0089a3c1866d
JafarLoka <gaafer.hajji1995@gmail.com>
2025-12-26 14:08:29 +0300

    Refactor to add validation

--- jloka-test/src/cats/dto/create-cat.dto.ts
+++ jloka-test/src/cats/dto/create-cat.dto.ts
@@ -1,9 +1,13 @@
-import {IsString, IsInt} from 'class-validator'
+import {IsString, IsInt, IsNotEmpty, Min, Max} from 'class-validator'
 export class CreateCatDto {
     @IsString()
+    @IsNotEmpty()
     name: string;
     @IsInt()
+    @Min(1)
+    @Max(50)
     age: number;
     @IsString()
+    @IsNotEmpty()
     breed: string;
 }
