100cc0cce9b8282526a6a050937b260acb7c3044
JafarLoka <gaafer.hajji1995@gmail.com>
2025-12-26 14:13:15 +0300

    Adding global validation pipe

--- jloka-test/src/main.ts
+++ jloka-test/src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CatsFilter } from './cats/cats.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
   app.useGlobalFilters(new CatsFilter())
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    forbidNonWhitelisted: true,
+  }))
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap();
