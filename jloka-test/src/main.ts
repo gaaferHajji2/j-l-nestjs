e61c782cb30dcc384cb2f9a3939245cae3eeb3cb
JafarLoka <gaafer.hajji1995@gmail.com>
2025-12-26 15:23:52 +0300

    Fix issue about exception filter

--- jloka-test/src/main.ts
+++ jloka-test/src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
-  app.useGlobalFilters(new CatsFilter())
+  // app.useGlobalFilters(new CatsFilter())
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
