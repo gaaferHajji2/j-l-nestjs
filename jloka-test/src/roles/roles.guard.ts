c691ed6797bc3df9afc89de3486297b441337527
JafarLoka <gaafer.hajji1995@gmail.com>
2025-12-27 15:15:26 +0300

    Remove unused lines

--- jloka-test/src/roles/roles.guard.ts
+++ jloka-test/src/roles/roles.guard.ts
@@ -5,26 +5,19 @@ import { Roles } from './roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-
   constructor(private readonly reflactor: Reflector) {}
 
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const roles = this.reflactor.get(Roles, context.getHandler())
-
-    console
-
     if(!roles) {
       return true
     }
 
     const request = context.switchToHttp().getRequest()
-
     const t1 = request.headers['roles']
-
     console.log("the roles of the user is: ", t1)
-
     return true;
   }
 }
