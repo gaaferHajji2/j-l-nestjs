13b58e37f9e49cef24d69a9fb70a4403e7431030
JafarLoka <gaafer.hajji1995@gmail.com>
2025-12-27 14:27:24 +0300

    Implement simple auth guard

---
+++ jloka-test/src/auth-guard/auth-guard.guard.ts
@@ -0,0 +1,18 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuardGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request: Request = context.switchToHttp().getRequest()
+
+    if(request.headers['Auth-Token'] == null) {
+      return false
+    }else {
+      return true
+    }
+  }
+}
