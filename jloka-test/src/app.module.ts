75d72763819daf4a139432f4646d57e55dd8c12b
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-12-28 14:11:54 +0300

    Refactor

--- jloka-test/src/app.module.ts
+++ jloka-test/src/app.module.ts
@@ -2,12 +2,11 @@ import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/c
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './users2/users.module';
 import { CatsModule } from './cats/cats.module';
 // import { CatsMiddleware } from './cats/cats.middleware';
 import { logger } from './cats/cats.middleware';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule, EventEmitterModule.forRoot(), CatsModule, AuthModule],
