568b3c55d6c0cf0c81850796c0a92b2517247057
JafarLoka <gaafer.hajji1995@gmail.com>
2025-12-30 12:01:34 +0300

    Init users

--- jloka-test/src/users/users.service.ts
+++ jloka-test/src/users/users.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
+// for real scenarios we must declare it using classes with class-validator
+export type User = any;
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    private readonly users = [
+    {
+      userId: 1,
+      username: 'Jafar',
+      password: 'Loka',
+    },
+    {
+      userId: 2,
+      username: 'Maria',
+      password: 'Loka',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find(user => user.username === username);
+  }
+
+}
