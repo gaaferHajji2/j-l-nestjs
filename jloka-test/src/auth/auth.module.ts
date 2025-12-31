df34e6529d67b1fa94c8eff370437482395e6cbe
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-12-31 11:41:24 +0300

    Import passport for auth module

--- jloka-test/src/auth/auth.module.ts
+++ jloka-test/src/auth/auth.module.ts
@@ -1,18 +1,20 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
-import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
+import { LocalStartegy } from './local.strategy'
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStartegy],
   imports: [UsersModule, JwtModule.register({
     global: true,
     secret: jwtConstants.secret,
     signOptions: { expiresIn: '3600s' }
-  })],
+  }), PassportModule],
   exports: [AuthService],
 })
 export class AuthModule {}
--- jloka-test/src/auth/local.strategy.ts
+++ jloka-test/src/auth/local.strategy.ts
@@ -8,7 +8,7 @@ export class LocalStartegy extends PassportStrategy(Strategy) {
         super();
     }
 
-    async validate(username: string, password: string) {
+    async validate(username: string, password: string): Promise<any> {
         const user = await this.authService.validateUser(username, password)
         if(!user) {
             throw new UnauthorizedException()
