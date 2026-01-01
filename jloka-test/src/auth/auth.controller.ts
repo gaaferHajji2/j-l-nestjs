72a34a906fc787dcc8f30855a4b86cbcbdacf0a9
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-01 15:04:27 +0300

    Init login passport method

--- jloka-test/src/auth/auth.controller.ts
+++ jloka-test/src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards }
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signin.dto';
 import { AuthGuard } from './auth.guard';
+import { AuthGuard as PassportAuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -11,6 +12,12 @@ export class AuthController {
     @Post('login')
     signIn(@Body() signInDto: SignInDto) {
         return this.authService.signIn(signInDto.username, signInDto.password);
+    }
+
+    @UseGuards(PassportAuthGuard('local'))
+    @Post('passport/login')
+    async login(@Request() req) {
+        return req.user
     }
 
     @UseGuards(AuthGuard)
