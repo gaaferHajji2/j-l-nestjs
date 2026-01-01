29b1be63e5a395779ee5289997867ef17048c0a2
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-01 15:56:01 +0300

    Basic extending of jwt auth guard

--- jloka-test/src/jwt-auth/jwt-auth.guard.ts
+++ jloka-test/src/jwt-auth/jwt-auth.guard.ts
@@ -1,7 +1,14 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    return super.canActivate(context)
+  }
+
+  handleRequest(err: any, user: any, info: any, context: ExecutionContext, status?: any) {
+    return user
+  }
 }
