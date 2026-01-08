6d36d409cc1d72f0fab0a4c783f93c1f92cc595a
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-08 14:16:35 +0300

    Refactor

--- jloka-test/src/auth/jwt.strategy.ts
+++ jloka-test/src/auth/jwt.strategy.ts
@@ -3,7 +3,6 @@ import { PassportStrategy } from "@nestjs/passport";
 import { Strategy, ExtractJwt } from "passport-jwt";
 import { jwtConstants } from "./constants";
 
-
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor() {
