5fda8e776330dcee8e1cdf93609d3253043d213e
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-08 14:00:08 +0300

    Adding graphql and create guard

--- jloka-test/src/user/user.decorator.ts
+++ jloka-test/src/user/user.decorator.ts
@@ -1,4 +1,5 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 
 export const User = createParamDecorator(
   (data: string, ctx: ExecutionContext) => {
@@ -6,5 +7,12 @@ export const User = createParamDecorator(
     console.log(`The request is: ${request.path}`);
     const user = { id: 1, username: 'JLoka-01' }
     return data ? user[data] : user;
+  },
+);
+
+export const CurrentUser = createParamDecorator(
+  (data: unknown, context: ExecutionContext) => {
+    const ctx = GqlExecutionContext.create(context)
+    return ctx.getContext().req.user
   },
 );
