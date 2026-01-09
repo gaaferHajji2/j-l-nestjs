12cdaa40406e0a584d03a12a842947973dbc1596
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-09 12:46:45 +0300

    Install typeorm module & init it for app module

--- jloka-test-typeorm/src/app.module.ts
+++ jloka-test-typeorm/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123',
+      database: 'test',
+      entities: [],
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
