120f218b47089821574ed45f6216e649df3be132
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-13 12:52:47 +0300

    Implement create many users with transactions

--- jloka-test-typeorm/src/users/users.service.ts
+++ jloka-test-typeorm/src/users/users.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
-import { DeleteResult, Repository } from 'typeorm';
+import { DataSource, DeleteResult, Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
 
     constructor(
-        @InjectRepository(User) private userRepository: Repository<User>
+        @InjectRepository(User) private userRepository: Repository<User>,
+        private dataSource: DataSource
     ) {}
 
     async findAll():Promise<User[]> {
@@ -20,5 +21,20 @@ export class UsersService {
 
     async remove(id: number): Promise<DeleteResult> {
         return this.userRepository.delete(id)
+    }
+
+    async createMany(users: User[]) {
+        const queryRunner = this.dataSource.createQueryRunner()
+        await queryRunner.connect()
+        await queryRunner.startTransaction()
+        try {
+            await queryRunner.manager.save(users[0])
+            await queryRunner.manager.save(users[1])
+            await queryRunner.commitTransaction()
+        } catch (e) {
+            await queryRunner.rollbackTransaction()
+        } finally {
+            await queryRunner.release()
+        }
     }
 }
