a7cb9a58ff1d6a55aba8124ba64166d654eba120
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-13 12:59:30 +0300

    Adding subscriber to user module

--- jloka-test-typeorm/src/users/users.module.ts
+++ jloka-test-typeorm/src/users/users.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { User } from './user.entity';
-import { UserSchema } from './user.schema';
+import { User } from './user.entity';
+import { UserSubscriber } from './users.subscriber';
+// import { UserSchema } from './user.schema';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserSchema])],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, UserSubscriber]
 })
 export class UsersModule {}
