579575e5071429a471b528dfcb1c9f5f4f91e06e
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-13 13:20:36 +0300

    Refactor to user async typeorm module

--- jloka-test-typeorm/src/app.module.ts
+++ jloka-test-typeorm/src/app.module.ts
@@ -5,20 +5,29 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 // import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
 import { Photo } from './photo/photo.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mariadb',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '123',
-      database: 'nestjs_relations',
-      autoLoadEntities: true,
-      entities: [Photo],
-      synchronize: false,
-      logging: true
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mariadb',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+        cli: {
+          migrationsDir: 'src/migrations',
+        },
+        synchronize: false,
+        logging: true,
+      }),
+      inject: [ConfigService],
     }),
     UsersModule
   ],
