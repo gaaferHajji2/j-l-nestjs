9fb3cea6527936c442e726664ffd5b25a314a1c7
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-18 14:40:59 +0300

    Create update category dto

---
+++ jloka-test-typeorm/src/category/dto/update-category.dto.ts
@@ -0,0 +1,21 @@
+import {
+  IsString,
+  IsOptional,
+  MinLength,
+  MaxLength,
+} from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCategoryDto } from './create-category.dto';
+
+export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
+  @IsOptional()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(200)
+  description?: string;
+}
