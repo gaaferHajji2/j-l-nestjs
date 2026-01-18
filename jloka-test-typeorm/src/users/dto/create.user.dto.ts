88ca9a98b077b13b2470cf674bbf61ee49309613
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-18 14:22:18 +0300

    Create user dto

--- jloka-test-typeorm/src/users/dto/create.user.dto.ts
+++ jloka-test-typeorm/src/users/dto/create.user.dto.ts
@@ -1,7 +1,36 @@
+import {
+  IsEmail,
+  IsString,
+  IsOptional,
+  IsBoolean,
+  MinLength,
+  MaxLength,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { CreateProfileDto } from '../../profile/dto/create-profile.dto';
+
 export class CreateUserDto {
-    email: string
-    firstName: string
-    lastName: string
-    isActive: boolean
-    profile: string
+  @IsEmail()
+  @MaxLength(255)
+  email: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
+  firstName: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
+  lastName: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => CreateProfileDto)
+  profile?: CreateProfileDto;
 }
