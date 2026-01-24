10f735ad5c339f0473ae73d661c4839346677776
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-24 14:01:28 +0300

    Adding serve static module && update app module

--- jloka-upload-files/src/app.module.ts
+++ jloka-upload-files/src/app.module.ts
@@ -3,10 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FileUploadService } from './file-upload/services/file-upload.service';
 import { FileUploadController } from './file-upload/file-upload.controller';
+import { ConfigModule } from '@nestjs/config';
+import { FileUploadValidator } from './file-upload/validation/file-upload-validator';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    },
+  ), 
+  ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
+  })
+],
   controllers: [AppController, FileUploadController],
-  providers: [AppService, FileUploadService],
+  providers: [AppService, FileUploadService, FileUploadValidator],
+
 })
-export class AppModule {}
+export class AppModule { }
