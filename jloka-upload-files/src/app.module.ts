ca12908b1ddca7ca9385f2bf10d19830df26d4c7
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-24 13:56:37 +0300

    Create file upload controller && organize files

--- jloka-upload-files/src/app.module.ts
+++ jloka-upload-files/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FileUploadService } from './file-upload/file-upload/file-upload.service';
+import { FileUploadService } from './file-upload/services/file-upload.service';
+import { FileUploadController } from './file-upload/file-upload.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, FileUploadController],
   providers: [AppService, FileUploadService],
 })
 export class AppModule {}
