cd1f00fd10cda09e1601455d87e8646cf3fb2e82
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-25 15:57:07 +0300

    Register CacheModule as Global with RedisStore

--- jloka-redis-caching/src/app.module.ts
+++ jloka-redis-caching/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import * as redisStore from 'cache-manager-redis-store';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register({
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+      isGlobal: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
