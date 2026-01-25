6731486acc13111bfe548aaa5f97218e5e93225d
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-25 15:59:41 +0300

    Generate items resource

--- jloka-redis-caching/src/app.module.ts
+++ jloka-redis-caching/src/app.module.ts
@@ -3,6 +3,7 @@ import * as redisStore from 'cache-manager-redis-store';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CacheModule } from '@nestjs/cache-manager';
+import { ItemsModule } from './items/items.module';
 
 @Module({
   imports: [
@@ -11,7 +12,8 @@ import { CacheModule } from '@nestjs/cache-manager';
       host: 'localhost',
       port: 6379,
       isGlobal: true
-    })
+    }),
+    ItemsModule
   ],
   controllers: [AppController],
   providers: [AppService],
