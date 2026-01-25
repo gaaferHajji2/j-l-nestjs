19b2de8ebab113631d74ffac9bbd046d42beb174
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-25 15:45:19 +0300

    Upload multiple files using File Fields Interceptor

--- jloka-upload-files/src/file-upload/file-upload.controller.ts
+++ jloka-upload-files/src/file-upload/file-upload.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { FileUploadService } from './file-upload.service';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileFieldsInterceptor, FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('file-upload')
 export class FileUploadController {
@@ -10,5 +10,22 @@ export class FileUploadController {
   @UseInterceptors(FileInterceptor('file'))
   uploadFile(@UploadedFile() file: Express.Multer.File) {
     return this.fileUploadService.handleFileUpload(file)
+  }
+
+  @Post('/upload-multiple')
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'main', maxCount: 1},
+      { name: 'avatar', maxCount: 1}
+    ])
+  )
+  uploadMultipleFiles(@UploadedFiles() files: {
+    main?: Express.Multer.File[], avatar?: Express.Multer.File[]
+  }){
+    return {
+      message: 'Files uploaded successfully!',
+      avatar: files.avatar?.[0].path,
+      background: files.main?.[0].path,
+    };
   }
 }
