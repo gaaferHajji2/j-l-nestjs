e629459019cb00f3c015f46610d826d7babf3b2a
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-26 14:24:04 +0300

    Update app service to use i18nservice

--- jloka-i18n-test/src/app.service.ts
+++ jloka-i18n-test/src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { I18nContext, I18nService } from 'nestjs-i18n';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly i18n: I18nService) {}
+  getHello(): Object {
+    return {
+      "1": this.i18n.t('test.HELLO', { lang: I18nContext?.current()?.lang }),
+      "2": this.i18n.t('test.PRODUCT.NEW', { args: { name: "Jafar Loka-01" }})
+    };
   }
+
 }
