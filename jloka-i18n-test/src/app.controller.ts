de291fa74a717222bbf35770b6edc83a0c8d114e
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-26 14:43:43 +0300

    Remove unused import

--- jloka-i18n-test/src/app.controller.ts
+++ jloka-i18n-test/src/app.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import { I18n, I18nContext } from 'nestjs-i18n';
 
 @Controller()
 export class AppController {
