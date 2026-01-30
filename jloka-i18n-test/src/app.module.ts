e6dd463e013185e501f329c39a756a8b003c8023
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-30 12:07:10 +0300

    Use validation for app

--- jloka-i18n-test/src/app.module.ts
+++ jloka-i18n-test/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { HeaderResolver, I18nJsonLoader, I18nModule, QueryResolver } from 'nestjs-i18n';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    I18nModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        fallbackLanguage: 'en',
+        loaderOptions: {
+          path: join(__dirname, '/i18n/'),
+          watch: configService.get('NODE_ENV') === 'development',
+        },
+      }),
+      loader: I18nJsonLoader,
+      resolvers: [
+        { use: QueryResolver, options: ['lang', 'locale'] },
+        new HeaderResolver(['x-custom-lang']),
+      ],
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
