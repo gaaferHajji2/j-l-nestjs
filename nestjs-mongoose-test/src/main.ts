a73c4d4bb55e33b782c477f8282c2fa68877ecc6
JafarLoka <gaafer.hajji1995@gmail.com>
2026-02-03 10:25:33 +0300

    Add configuration for swagger & validation pipe & cors

--- nestjs-mongoose-test/src/main.ts
+++ nestjs-mongoose-test/src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+    // Global validation pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  // Swagger documentation
+  const config = new DocumentBuilder()
+    .setTitle('Library Management API')
+    .setDescription('NestJS + MongoDB with Dynamic Population')
+    .setVersion('1.0')
+    .addTag('Library')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  // Enable CORS
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
