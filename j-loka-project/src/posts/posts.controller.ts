ea9aa9b3a93561cf7bd40c52bc0da83842dc83af
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-28 02:41:03 +0300

    Adding the api response and operation for creating a new post

--- j-loka-project/src/posts/posts.controller.ts
+++ j-loka-project/src/posts/posts.controller.ts
@@ -2,6 +2,7 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common'
 import { PostsService } from './posts.service'
 import { CreatePostDto } from './dto/create-post.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('posts')
 export class PostsController {
@@ -17,6 +18,13 @@ export class PostsController {
         return this.postsService.getAllPosts();
     }
 
+    @ApiOperation({
+        summary: "Creates A New Blog Post"
+    })
+    @ApiResponse({
+        status: 201,
+        description: "201 if the post created successfully"
+    })
     @Post('/')
     public createPost(@Body() createPostDto: CreatePostDto) {
         return createPostDto;
