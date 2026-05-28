1b9647d4872cd22e3151e87a0dab2f75fa2f4cea
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-28 11:49:05 +0300

    Init app module with typeorm module --without full option

--- j-loka-project/src/app.module.ts
+++ j-loka-project/src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { PostsModule } from './posts/posts.module';
-import { AuthModule } from './auth/auth.module';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { UsersModule } from './users/users.module'
+import { PostsModule } from './posts/posts.module'
+import { AuthModule } from './auth/auth.module'
+
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [UsersModule, PostsModule, AuthModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    entities: []
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
